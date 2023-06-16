587b3abd003577d965b5475745f5528f67d8f9a6
dayvid.silva <dayvid.silva@mds.gov.br>
2023-06-15 23:48:18 -0300

    Adiciona o Swagger ao projeto

--- laravel-project/app/Http/Controllers/ProdutoController.php
+++ laravel-project/app/Http/Controllers/ProdutoController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * * @OA\Info(
+ *     version="1.0",
+ *     title="Example for response examples value"
+ * )
+ * @OA\Get(
+ *     tags={"produtos"},
+ *     summary="Returns a list of products",
+ *     description="Returns a object of products",
+ *     path="/teste",
+ *     @OA\Response(response="200", description="A list with courses"),
+ * ),
+ * 
+ */
+
 class ProdutoController extends Controller
 {
+
     public function teste()
     {
         $response = ['msg' => 'teste'];
